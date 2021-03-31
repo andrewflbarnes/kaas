67627ff55514904ab8de9b0c49c4dc4ffedc251c
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2021-03-31 01:21:20 +0100

    Cleanup group configuration and turn into a safer enum form

--- kaas-core/src/main/java/net/aflb/kaas/core/legacy/races/group/GroupConfiguration.java
+++ kaas-core/src/main/java/net/aflb/kaas/core/legacy/races/group/GroupConfiguration.java
@@ -1,11 +1,10 @@
-/**
- *
- */
 package net.aflb.kaas.core.legacy.races.group;
 
 import lombok.extern.slf4j.Slf4j;
 import net.aflb.kaas.core.model.Team;
 import net.aflb.kaas.core.model.competing.Match;
+
+import java.util.Arrays;
 
 /**
  * <p>
@@ -31,65 +30,80 @@ import net.aflb.kaas.core.model.competing.Match;
  * @author Barnesly
  */
 @Slf4j
-public class GroupConfiguration {
-	// Each of the race grids is composed of an array containing 3 arrays (1 per
-	// section) of race pairs - each themselves a 2 dimensional array
-	public static final GroupConfiguration TWO = new GroupConfiguration("2", new int[][][] {
-			{ { 1, 2 } },
-			{ { 2, 1 } },
-			{ { 1, 2 } } });
-	public static final GroupConfiguration KNOCKOUT = new GroupConfiguration("2F", new int[][][] {
-			{ { 1, 2 } }});
-	public static final GroupConfiguration THREE = new GroupConfiguration("3", new int[][][] {
-			{ { 1, 2 } },
-			{ { 2, 3 } },
-			{ { 3, 1 } } });
-	public static final GroupConfiguration FOUR = new GroupConfiguration("4", new int[][][] {
-			{ { 1, 2 }, { 3, 4 } },
-			{ { 2, 3 }, { 4, 1 } },
-			{ { 1, 3 }, { 2, 4 } } });
-	public static final GroupConfiguration FOUR_SPECIAL = new GroupConfiguration("4S", new int[][][] {
-			{ { 1, 2 }, { 3, 4 }, { 2, 3 }, { 4, 1 } },
-			{ { 1, 3 }, { 2, 4 }, { 2, 1 }, { 4, 3 } },
-			{ { 3, 2 }, { 1, 4 }, { 3, 1 }, { 4, 2 } } });
-	public static final GroupConfiguration FIVE_SPECIAL = new GroupConfiguration("5S", new int[][][] {
-			{ { 1, 2 }, { 3, 4 }, { 4, 5 } },
-			{ { 2, 3 }, { 5, 1 }, { 4, 2 } },
-			{ { 5, 3 }, { 1, 4 }, { 2, 5 }, { 3, 1 } } });
-	public static final GroupConfiguration SIX_SPECIAL = new GroupConfiguration("6S", new int[][][] {
-			{ { 1, 2 }, { 3, 4 }, { 5, 6 }, { 2, 3 }, { 6, 1 } },
-			{ { 4, 5 }, { 3, 1 }, { 4, 6 }, { 5, 2 }, { 1, 4 } },
-			{ { 2, 6 }, { 3, 5 }, { 5, 1 }, { 4, 2 }, { 6, 3 } } });
+public enum GroupConfiguration {
 
-	private final String value;
-	private final int[][][] raceGrid;
+    // Each of the race grids is composed of an array containing 3 arrays (1 per
+    // section) of race pairs - each themselves a 2 dimensional array
+    TWO("2", 2, new int[][][]{
+            {{1, 2}},
+            {{2, 1}},
+            {{1, 2}}}),
+    THREE("3", 3, new int[][][]{
+            {{1, 2}},
+            {{2, 3}},
+            {{3, 1}}}),
+    FOUR("4", 4, new int[][][]{
+            {{1, 2}, {3, 4}},
+            {{2, 3}, {4, 1}},
+            {{1, 3}, {2, 4}}}),
+    FOUR_SPECIAL("4S", 4, new int[][][]{
+            {{1, 2}, {3, 4}, {2, 3}, {4, 1}},
+            {{1, 3}, {2, 4}, {2, 1}, {4, 3}},
+            {{3, 2}, {1, 4}, {3, 1}, {4, 2}}}),
+    FIVE_SPECIAL("5S", 5, new int[][][]{
+            {{1, 2}, {3, 4}, {4, 5}},
+            {{2, 3}, {5, 1}, {4, 2}},
+            {{5, 3}, {1, 4}, {2, 5}, {3, 1}}}),
+    SIX_SPECIAL("6S", 6, new int[][][]{
+            {{1, 2}, {3, 4}, {5, 6}, {2, 3}, {6, 1}},
+            {{4, 5}, {3, 1}, {4, 6}, {5, 2}, {1, 4}},
+            {{2, 6}, {3, 5}, {5, 1}, {4, 2}, {6, 3}}}),
+    KNOCKOUT("2F", 2, new int[][][]{
+            {{1, 2}}});
 
-	/**
-	 * The constructor
-	 *
-	 * @param value
-	 *            the value associated with this configuration. The first digit
-	 *            must be the number of teams in this group
-	 * @param raceGrid
-	 *            the multi-dimensional integer array defining the races
-	 *            which will be run for this number of teams
-	 */
-	private GroupConfiguration(String value, int[][][] raceGrid) {
-		this.value = value;
-		this.raceGrid = raceGrid;
-	}
-	/**
-	 * @return the number of teams competing under this configuration
-	 */
-	public int teamCount() {
-		return Integer.parseInt(this.value.substring(0, 1));
-	}
+    private final String alias;
+    private final int teamCount;
+    private final int[][][] grid;
 
-	/**
-	 * @return the multi-dimensional numeric array defining the races which will
-	 *         be run
-	 */
-	public int[][][] getRaceGrid() {
-		return this.raceGrid;
-	}
+    GroupConfiguration(String alias, int teamCount, int[][][] grid) {
+        this.alias = alias;
+        this.teamCount = teamCount;
+        this.grid = grid;
+    }
+
+    /**
+     * @return the alias name for this configuration
+     */
+    public String getName() {
+        return alias;
+    }
+
+    /**
+     * @return the number of teams competing under this configuration
+     */
+    public int teamCount() {
+        return teamCount;
+    }
+
+    /**
+     * @return the multi-dimensional numeric array defining the races which will
+     *         be run
+     */
+    public int[][][] getRaceGrid() {
+        return copy(grid);
+    }
+
+    private int[][][] copy(final int[][][] original) {
+        final var iLength = original.length;
+        final var copy = new int[iLength][][];
+        for (int i = 0; i < iLength; i++) {
+            final var jLength = original[i].length;
+            copy[i] = new int[jLength][];
+            for (int j = 0; j < jLength; j++) {
+                copy[i][j] = Arrays.copyOf(original[i][j], original[i][j].length);
+            }
+        }
+
+        return copy;
+    }
 }
