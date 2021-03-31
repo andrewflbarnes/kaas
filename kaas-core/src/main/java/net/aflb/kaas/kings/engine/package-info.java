100f0d9caafbaf0050d16f0f1911fe10495dc2b8
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2021-03-31 01:24:24 +0100

    Move a package-info to the correct package

--- kaas-core/src/main/java/net/aflb/kaas/kings/engine/package-info.java
+++ kaas-core/src/main/java/net/aflb/kaas/kings/engine/package-info.java
@@ -2,4 +2,4 @@
  * Contents to be moved to kaas-engine one refactoring work is complete.
  * Maybe we should actually move it to kaas-kings-engine? That way we can define the SPI in kaas-core without?
  */
-package net.aflb.kaas.engine;
+package net.aflb.kaas.kings.engine;
